a4b054127eb308b038f88772cc965c1df0ee5e28
mattbelsky <matt.belsky@gmail.com>
2018-07-09 22:15:36 +0800

    swap the value of two string variables with no third temporary variable

---
+++ SwapTwoValuesNoThirdVar.java
@@ -0,0 +1,23 @@
+/**
+ * Swaps the values of two string variables without using a third temporary variable.
+ */
+class SwapTwoValuesNoThirdVar {
+
+	public static void main(String[] args) {
+
+		String a = "howdy";
+		String b = "pardner";
+
+		System.out.println("a = " + a + "\nb = " + b);
+
+		int lengthA = a.length();
+		int lengthB = b.length();
+		
+		a = a.concat(b);
+		b = a.substring(0, lengthA);
+		a = a.substring(lengthA);
+
+		System.out.println("a = " + a + "\nb = " + b);
+
+	}
+}
